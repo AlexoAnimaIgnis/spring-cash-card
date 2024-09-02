a217c376898ea90dc648da6de0e204eba739c40b
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2024-09-02 23:58:58 +0800

    implemented PathVariable

--- src/main/java/com/example/cashcard/CashCardController.java
+++ src/main/java/com/example/cashcard/CashCardController.java
@@ -23,8 +23,17 @@ public class CashCardController {
      * requests that match /cashcards/{requestedID}
      */
     @GetMapping("/{requestedId}")
-    public ResponseEntity<CashCard> findById() {
-        CashCard cashCard = new CashCard(99L, 123.45);
-        return ResponseEntity.ok(cashCard);
+    public ResponseEntity<CashCard> findById(
+            /**
+             * PathVariable makes Spring Web aware of the requestedID supplied in the HTTP request
+             */
+            @PathVariable Long requestedId) {
+
+        if (requestedId.equals(99L)) {
+            CashCard cashCard = new CashCard(99L, 123.45);
+            return ResponseEntity.ok(cashCard);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
